f5672f900c1a66b3835f1637dc33a3cbdf3a1a96
yueyong <1397604589@qq.com>
2019-04-02 17:09:35 +0800

    【修改】

--- src/routes/commpent/pie/index.js
+++ src/routes/commpent/pie/index.js
@@ -1,49 +1,47 @@
 import React, { Component } from 'react'
-import { connect } from 'dva';
-import { Card, Form } from 'antd';
 import Echart from 'echarts';
 import './index.less'
+import {cloneDeep} from 'lodash'
 class Index extends Component {
     constructor(props) {
         super(props)
 
     }
     componentWillReceiveProps = (nextProps) => {
-        let data = nextProps.data
+        let data =cloneDeep(nextProps.data)
         let item = nextProps.item
         data.sort((a, b) => {
             if (a.value > b.value) return -1
             if (a.value < b.value) return 1
             else return 0
         })
-        let title = item > 0 ? data[item - 1].key : data[data.length + item].key
-        let num = item > 0 ? data[item - 1].value : data[data.length + item].value
+        let title = data.length > 3 ? item > 0 ? data[item - 1].key : data[data.length + item].key : ''
+        let num = data.length > 3 ? item > 0 ? data[item - 1].value : data[data.length + item].value : ''
         let option = {
             tooltip: {
+                show:false,
                 trigger: 'item',
                 formatter: "{a} <br/>{b}: {c} ({d}%)"
             },
             color: ["#00c0ef"],
             series: [
                 {
-                    name: '访问来源',
+                    name: title,
                     type: 'pie',
                     radius: ['75%', '85%'],
                     avoidLabelOverlap: false,
-                    // hoveranination:false,
-                    // silent:true,
                     label: {
                         normal: {
                             show: true,
                             position: 'center',
                             formatter: function (argument) {
                                 var html;
-                                html = num+'\r\n\r\n' + title;
+                                html = num + '\r\n\r\n' + title;
                                 return html;
                             },
                             textStyle: {
                                 fontSize: 15,
-                                color: '#00c0ef'
+                                color: '#FFFFFF'
                             }
                         }
                     },
@@ -53,7 +51,7 @@ class Index extends Component {
                         }
                     },
                     data: [
-                        { value: 400, name: '本月业绩' }
+                        { value: num, name: '本月业绩' },
                     ]
                 }
             ]
--- src/routes/mult/index.js
+++ src/routes/mult/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'dva';
-import { Divider } from 'antd'
+import { Divider, Carousel } from 'antd'
 import SortDown from '../../assets/icon_sort_down.png'
 import SortUp from '../../assets/icon_sort_up.png'
 import POST from '../../utils/request.js'
-import Bar from '../commpent/bar/index';
+import Bar from '../commpent/mulBar/index';
 import Pie from '../commpent/pie/index';
+import Btn_white from '../../assets/btn_white.png'
+import Btn_gao from '../../assets/btn_gao.png'
+import Btn_zhong from '../../assets/btn_zhong.png'
+import Btn_di from '../../assets/btn_di.png'
 import './index.less'
 const length = 0;
 class Index extends Component {
@@ -14,7 +18,12 @@ class Index extends Component {
         this.state = {
             index: 1,
             barIndex: 1,
-            data: [],
+            gao: [],
+            zhong: [],
+            di: [],
+            gaoBtn: 1,
+            zhongBtn: 1,
+            diBtn: 1,
             sortType: 0
         }
     }
@@ -53,30 +62,151 @@ class Index extends Component {
     //排序点击
     sort = (e) => {
         e.stopPropagation()
-        let { data, sortType } = this.state
-        data.sort((a, b) => {
-            if (a.value > b.value) return sortType ? -1 : 1
-            if (a.value < b.value) return sortType ? 1 : -1
-            else return 0
-        })
-        this.setState({
-            data: data,
-            sortType: sortType ? 0 : 1
-        })
+        let { gao, zhong, di, sortType, gaoBtn, zhongBtn, diBtn } = this.state
+        if (gaoBtn) {
+            gao.sort((a, b) => {
+                if (a.value > b.value) return sortType ? -1 : 1
+                if (a.value < b.value) return sortType ? 1 : -1
+                else return 0
+            })
+            let zhong_new = []
+            let di_new = []
+            gao.map((ele) => {
+                zhong.map((zhong_e) => {
+                    if (ele.key == zhong_e.key) {
+                        zhong_new.push(zhong_e)
+                    }
+                })
+                di.map((di_e) => {
+                    if (ele.key == di_e.key) {
+                        di_new.push(di_e)
+                    }
+                })
+            })
+            this.setState({
+                gao,
+                zhong: zhong_new,
+                di: di_new,
+                sortType: sortType ? 0 : 1
+
+            })
+        }
+        if (zhongBtn) {
+           
+            zhong.sort((a, b) => {
+                if (a.value > b.value) return sortType ? -1 : 1
+                if (a.value < b.value) return sortType ? 1 : -1
+                else return 0
+            })
+            let gao_new = []
+            let di_new = []
+            zhong.map((ele) => {
+                gao.map((gao_e) => {
+                    if (ele.key == gao_e.key) {
+                        gao_new.push(gao_e)
+                    }
+                })
+                di.map((di_e) => {
+                    if (ele.key == di_e.key) {
+                        di_new.push(di_e)
+                    }
+                })
+            })
+            this.setState({
+                gao:gao_new,
+                zhong: zhong,
+                di: di_new,
+                sortType: sortType ? 0 : 1
+
+            })
+        }
+        if (diBtn) {
+            di.sort((a, b) => {
+                if (a.value > b.value) return sortType ? -1 : 1
+                if (a.value < b.value) return sortType ? 1 : -1
+                else return 0
+            })
+            let zhong_new = []
+            let gao_new = []
+            di.map((ele) => {
+                zhong.map((zhong_e) => {
+                    if (ele.key == zhong_e.key) {
+                        zhong_new.push(zhong_e)
+                    }
+                })
+                gao.map((gao_e) => {
+                    if (ele.key == gao_e.key) {
+                        gao_new.push(gao_e)
+                    }
+                })
+            })
+            this.setState({
+                gao:gao_new,
+                zhong: zhong_new,
+                di: di,
+                sortType: sortType ? 0 : 1
+
+            })
+        }
     }
     postItem = (item) => {
-        POST('/demo/getCsv.php', { name: '总体手机', item: item }).then(app => {
+        POST('/demo/getCsv.php', { name: '高端手机', item: item }).then(app => {
             if (app.code == 0) {
                 this.setState({
-                    data: app.result
+                    gao: app.result
+                })
+            }
+        })
+        POST('/demo/getCsv.php', { name: '中端手机', item: item }).then(app => {
+            if (app.code == 0) {
+                this.setState({
+                    zhong: app.result
+                })
+            }
+        })
+        POST('/demo/getCsv.php', { name: '低端手机', item: item }).then(app => {
+            if (app.code == 0) {
+                this.setState({
+                    di: app.result
                 })
             }
         })
     }
+    gaoClick = () => {
+        this.setState({
+            gaoBtn: this.state.gaoBtn ? 0 : 1
+        })
+    }
+    zhongClick = () => {
+        this.setState({
+            zhongBtn: this.state.zhongBtn ? 0 : 1
+        })
+    }
+    diClick = () => {
+        this.setState({
+            diBtn: this.state.diBtn ? 0 : 1
+        })
+    }
+    Sortimg = () => {
+        const { gaoBtn, zhongBtn, diBtn } = this.state
+        if (gaoBtn) {
+            if (zhongBtn == 0 && diBtn == 0) return true
+        }
+        if (zhongBtn) {
+            if (gaoBtn == 0 && diBtn == 0) return true
+        }
+        if (diBtn) {
+            if (zhongBtn == 0 && gaoBtn == 0) return true
+        }
+        return false
+    }
     componentWillMount = () => {
-        this.postItem(1)
+        this.postItem(1);
+
     }
     render() {
+        const { gaoBtn, zhongBtn, diBtn } = this.state
+        let show = this.Sortimg()
         return (
             <div className='mult-body' >
                 <div className='table'><span className='table-title'>手机产品属性情感分析结果</span>
@@ -85,59 +215,158 @@ class Index extends Component {
                     <div className='table-button btn-three' onClick={this.btnThree} style={{ 'background-image': this.state.barIndex == 3 ? 'linear-gradient(-180deg, #00FFDE 0%, #004EFF 100%)' : null }}>情感方差</div>
                     <div className='table-button btn-four' onClick={this.btnFour} style={{ 'background-image': this.state.barIndex == 4 ? 'linear-gradient(-180deg, #00FFDE 0%, #004EFF 100%)' : null }}>需改进度</div>
                 </div>
-                <div className='sort'><img src={!this.state.sortType ? SortDown : SortUp} onClick={this.sort} /></div>
+                <div className='sort'>{show ? <img src={!this.state.sortType ? SortDown : SortUp} onClick={this.sort} /> : null}</div>
                 <div className='tables'>
-                    <Bar data={this.state.data} />
+                    <Bar gao={this.state.gao} zhong={this.state.zhong} di={this.state.di} btngao={gaoBtn} btnzhong={zhongBtn} btndi={diBtn} />
                     <Divider type='vertical' className='tables-divider' />
                     <div className='tables-Right'>
-                        <div>高端手机</div>
+                        <div>
+                            <span className='title'>高端手机</span>
+                            <img className='btn-1' src={!gaoBtn ? Btn_white : Btn_gao} onClick={this.gaoClick} />
+                            <span className='desc'>*价格在2500元以上的手机为高端手机</span>
+                            <span className='number'><span className='number-number'>22 </span> 手机个数</span>
+                        </div>
                         <Divider className='tables-Right-divider' />
-                        <div>中端手机</div>
+                        <div>
+                            <span className='title'>中端手机</span>
+                            <img className='btn-2' src={!zhongBtn ? Btn_white : Btn_zhong} onClick={this.zhongClick} />
+                            <span className='desc'> *价格在1500-2500元之间的手机为中端手机</span>
+                            <span className='number'><span className='number-number'>19 </span>  手机个数</span>
+                        </div>
                         <Divider className='tables-Right-divider' />
-                        <div>低端手机</div>
+                        <div>
+                            <span className='title'>低端手机</span>
+                            <img className='btn-3' src={!diBtn ? Btn_white : Btn_di} onClick={this.diClick} />
+                            <span className='desc'>*价格在1500元以下的手机为低端手机</span>
+                            <span className='number'><span className='number-number'>19 </span>  手机个数</span>
+                        </div>
                     </div>
                 </div>
                 <Divider className='divider' />
-                <div className='pie'>
-                    <div className='left'>
-                        <span className='left-title'>
-                            {
-                                this.state.barIndex == 1 ?
-                                    '满意度最高的产品属性' :
-                                    this.state.barIndex == 2 ?
-                                        '关注度最高的产品属性' :
-                                        this.state.barIndex == 3 ?
-                                            '情感房差最高的产品属性' :
-                                            '需改进度最高的产品属性'
-                            }
-                        </span>
-                        <div className='left-body'>
-                            <Pie className='left-pie one' data={this.state.data} item={1} />
-                            <Pie className='left-pie two' data={this.state.data} item={2} />
-                            <Pie className='left-pie three' data={this.state.data} item={3} />
+                <Carousel vertical className='Carousel'>
+                    <div className='pie'>
+                        <div className='left'>
+                            <span className='left-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-高端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-高端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-高端' :
+                                                '需改进度最高的产品属性-高端'
+                                }
+                            </span>
+                            <div className='left-body'>
+                                <Pie className='left-pie' data={this.state.gao} item={1} />
+                                <Pie className='left-pie' data={this.state.gao} item={2} />
+                                <Pie className='left-pie' data={this.state.gao} item={3} />
+                            </div>
                         </div>
-                    </div>
-                    <Divider type='vertical' className='divider-pie' />
-                    <div className='right'>
-                        <span className='right-title'>
-                            {
-                                this.state.barIndex == 1 ?
-                                    '满意度最高的产品属性' :
-                                    this.state.barIndex == 2 ?
-                                        '关注度最高的产品属性' :
-                                        this.state.barIndex == 3 ?
-                                            '情感房差最高的产品属性' :
-                                            '需改进度最高的产品属性'
-                            }
-                        </span>
-                        <div className='right-body'>
-                            <Pie className='right-pie one' data={this.state.data} item={-1} />
-                            <Pie className='right-pie two' data={this.state.data} item={-2} />
-                            <Pie className='right-pie three' data={this.state.data} item={-3} />
-                        </div>
+                        <Divider type='vertical' className='divider-pie' />
+                        <div className='right'>
+                            <span className='right-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-高端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-高端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-高端' :
+                                                '需改进度最高的产品属性-高端'
+                                }
+                            </span>
+                            <div className='right-body'>
+                                <Pie className='right-pie' data={this.state.gao} item={-1} />
+                                <Pie className='right-pie' data={this.state.gao} item={-2} />
+                                <Pie className='right-pie' data={this.state.gao} item={-3} />
+                            </div>
 
+                        </div>
                     </div>
-                </div>
+                    <div className='pie'>
+                        <div className='left'>
+                            <span className='left-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-中端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-中端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-中端' :
+                                                '需改进度最高的产品属性-中端'
+                                }
+                            </span>
+                            <div className='left-body'>
+                                <Pie className='left-pie' data={this.state.zhong} item={1} />
+                                <Pie className='left-pie' data={this.state.zhong} item={2} />
+                                <Pie className='left-pie' data={this.state.zhong} item={3} />
+                            </div>
+                        </div>
+                        <Divider type='vertical' className='divider-pie' />
+                        <div className='right'>
+                            <span className='right-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-中端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-中端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-中端' :
+                                                '需改进度最高的产品属性-中端'
+                                }
+                            </span>
+                            <div className='right-body'>
+                                <Pie className='right-pie' data={this.state.zhong} item={-1} />
+                                <Pie className='right-pie' data={this.state.zhong} item={-2} />
+                                <Pie className='right-pie' data={this.state.zhong} item={-3} />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className='pie'>
+                        <div className='left'>
+                            <span className='left-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-低端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-低端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-低端' :
+                                                '需改进度最高的产品属性-低端'
+                                }
+                            </span>
+                            <div className='left-body'>
+                                <Pie className='left-pie' data={this.state.di} item={1} />
+                                <Pie className='left-pie' data={this.state.di} item={2} />
+                                <Pie className='left-pie' data={this.state.di} item={3} />
+                            </div>
+                        </div>
+                        <Divider type='vertical' className='divider-pie' />
+                        <div className='right'>
+                            <span className='right-title'>
+                                {
+                                    this.state.barIndex == 1 ?
+                                        '满意度最高的产品属性-低端' :
+                                        this.state.barIndex == 2 ?
+                                            '关注度最高的产品属性-低端' :
+                                            this.state.barIndex == 3 ?
+                                                '情感房差最高的产品属性-低端' :
+                                                '需改进度最高的产品属性-低端'
+                                }
+                            </span>
+                            <div className='right-body'>
+                                <Pie className='right-pie' data={this.state.di} item={-1} />
+                                <Pie className='right-pie' data={this.state.di} item={-2} />
+                                <Pie className='right-pie' data={this.state.di} item={-3} />
+                            </div>
+
+                        </div>
+                    </div>
+                </Carousel>
+
             </div >
         )
     }
